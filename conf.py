d0c4dde2b8f4683f07978b67216fa4fa244831a4
Tomas F. Llano Rios <tllano11@gmail.com>
2025-05-16 15:19:28 -0400

    Adding sphinx tabs support

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ author = 'ITS - Research Computing'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_tabs.tabs']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
